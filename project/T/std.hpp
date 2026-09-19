0a6f3bbc44f124dbff43daab81cd070cd022e03d
c-7777 <congyuqing@tutamail.com>
2026-09-19 18:12:37 +0800

    commit

---
+++ project/T/std.hpp
@@ -0,0 +1,9 @@
+basic type
+_____________________________________________
+|void|    |           |           |          |
+|    |bool|short short|           |short char|
+|    |    |short      |           |char      |
+|    |    |int        |short float|long char |
+|    |    |long       |float      |          |
+|    |    |long long  |long float |          |
+|____________________________________________|
