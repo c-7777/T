934f4027f7fb273bfd391772a915484490d6ccbd
c-7777 <congyuqing@tutamail.com>
2026-09-26 12:43:10 +0800

    commit

--- project/T/std.hpp
+++ project/T/std.hpp
@@ -1,9 +1,31 @@
-basic type
-_____________________________________________
-|void|    |           |           |          |
-|    |bool|short short|           |short char|
-|    |    |short      |           |char      |
-|    |    |int        |short float|long char |
-|    |    |long       |float      |          |
-|    |    |long long  |long float |          |
-|____________________________________________|
+基本类型
+void 无型，默认0位
+bool 布尔型，默认8位
+char 字符型，默认16位
+int 整型，默认32位
+float 浮点型，默认64位
+
+扩展基本类型
+_half_float，半精度浮点型，默认16位
+_decimal_float，十进制浮点型，默认64位
+
+类型修饰符
+unsigned 类型无符号，对int类型生效
+short 类型位数除以2，对char, int, float, _decimal_float生效，对int可嵌套2层
+long 类型位数乘以2，对char, int, float, _decimal_float生效，对int可嵌套2层
+const 类型不可变，对任何类型生效
+
+
+
+附：关键字表
+_decimal_float
+_half_float
+bool
+char
+const
+float
+int
+long
+short
+unsigned
+void
